20b672584ef26947f38fbc96d23260cc880f2324
Derek.lo <derek.lo@flipsdigital.com>
2021-01-26 15:32:29 +0800

    cors 2

--- api/seo.js
+++ api/seo.js
@@ -1,10 +1,5 @@
 module.exports = function (req, res, next) {
-    const redirect = redirects.find(r => r.from === req.url)
-    if (redirect) {
-        res.end()
-    } else {
-        res.setHeader('Referrer-Policy', 'origin-when-cross-origin')
-        res.setHeader('X-Frame-Options', 'SAMEORIGIN')
-        next()
-    }
+    res.setHeader('Referrer-Policy', 'origin-when-cross-origin')
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN')
+    next()
 }
