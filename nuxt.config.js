c8eecce825c326f68abb757cb0e45b2a455eff54
Derek.lo <derek.lo@flipsdigital.com>
2021-01-26 14:57:26 +0800

    vercel deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,10 +27,6 @@ export default {
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-  generate: {
-    dir: 'dist'
-  },
-  buildDir: 'dist',
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [{
     src: '~plugins/leaflet.js',
