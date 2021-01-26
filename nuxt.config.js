3e36180ec28238bea4fe7d18691fed24e31c5182
Derek.lo <derek.lo@flipsdigital.com>
2021-01-26 14:52:16 +0800

    proxy build

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   generate: {
     dir: 'dist'
   },
+  buildDir: 'dist',
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [{
     src: '~plugins/leaflet.js',
